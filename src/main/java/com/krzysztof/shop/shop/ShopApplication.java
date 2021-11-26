87e06ee438a0848f356f515341efb63b91b26a65
KrzysztofStasiak <krzystof@kursGIT.pl>
2021-11-26 19:46:42 +0100

    fix

--- src/main/java/com/krzysztof/shop/shop/ShopApplication.java
+++ src/main/java/com/krzysztof/shop/shop/ShopApplication.java
@@ -43,6 +43,50 @@ public class ShopApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
+        List<Author> authorList = authorService.findAll();
+        Author author1 = new Author("Jan", "Kowalski");
+        Author author2 = new Author("Krzysztof", "Kula");
+        Author author3 = new Author("Kamil", "But");
+
+        if (authorList.size() == 0) {
+            authorService.saveAuthor(author1);
+            authorService.saveAuthor(author2);
+            authorService.saveAuthor(author3);
+        }
+
+        List<Category> categoryList = categoryService.findAll();
+        Category furniture = new Category("Furniture");
+        Category armchairs = new Category("Armchairs", furniture);
+        Category wardrobes = new Category("Wardrobes", furniture);
+        Category lawson = new Category("Lawson", armchairs);
+        Category club = new Category("Club", armchairs);
+        Category wood = new Category("Wood", wardrobes);
+        Category composite = new Category("Composite", wardrobes);
+
+        Category homeAppliances = new Category("HomeAppliances");
+        Category ovens = new Category("ovens", homeAppliances);
+        Category refrigerators = new Category("refrigerators", homeAppliances);
+
+        Category lighting = new Category("lighting");
+        Category ceilingLighting = new Category("Ceiling lighting", lighting);
+        Category wallLighting = new Category("Wall lighting", lighting);
+
+        if (categoryList.size() == 0) {
+            categoryService.save(furniture);
+            categoryService.save(armchairs);
+            categoryService.save(wardrobes);
+            categoryService.save(lawson);
+            categoryService.save(club);
+            categoryService.save(wood);
+            categoryService.save(composite);
+            categoryService.save(homeAppliances);
+            categoryService.save(ovens);
+            categoryService.save(refrigerators);
+            categoryService.save(lighting);
+            categoryService.save(ceilingLighting);
+            categoryService.save(wallLighting);
+        }
+
 
     }
 }
