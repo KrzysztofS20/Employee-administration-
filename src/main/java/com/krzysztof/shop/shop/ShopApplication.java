cabe464f64964163f89dae4294637c58aa4a0046
KrzysztofStasiak <krzystof@kursGIT.pl>
2021-11-26 19:35:32 +0100

    fix

--- src/main/java/com/krzysztof/shop/shop/ShopApplication.java
+++ src/main/java/com/krzysztof/shop/shop/ShopApplication.java
@@ -43,6 +43,104 @@ public class ShopApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
+        List<Author> authorList = authorService.findAll();
+        Author author1 = new Author("Jan", "Kowalski");
+        Author author2 = new Author("Krzysztof", "Kula");
+        Author author3 = new Author("Kamil", "But");
 
+        if (authorList.size() == 0) {
+            authorService.saveAuthor(author1);
+            authorService.saveAuthor(author2);
+            authorService.saveAuthor(author3);
+        }
+
+        List<Category> categoryList = categoryService.findAll();
+        Category furniture = new Category("Furniture");
+        Category armchairs = new Category("Armchairs", furniture);
+        Category wardrobes = new Category("Wardrobes", furniture);
+        Category lawson = new Category("Lawson", armchairs);
+        Category club = new Category("Club", armchairs);
+        Category wood = new Category("Wood", wardrobes);
+        Category composite = new Category("Composite", wardrobes);
+
+        Category homeAppliances = new Category("HomeAppliances");
+        Category ovens = new Category("ovens", homeAppliances);
+        Category refrigerators = new Category("refrigerators", homeAppliances);
+
+        Category lighting = new Category("lighting");
+        Category ceilingLighting = new Category("Ceiling lighting", lighting);
+        Category wallLighting = new Category("Wall lighting", lighting);
+
+        if (categoryList.size() == 0) {
+            categoryService.save(furniture);
+            categoryService.save(armchairs);
+            categoryService.save(wardrobes);
+            categoryService.save(lawson);
+            categoryService.save(club);
+            categoryService.save(wood);
+            categoryService.save(composite);
+            categoryService.save(homeAppliances);
+            categoryService.save(ovens);
+            categoryService.save(refrigerators);
+            categoryService.save(lighting);
+            categoryService.save(ceilingLighting);
+            categoryService.save(wallLighting);
+        }
+
+        List<Product> productList = productService.findAll();
+        Product lawsonBlue = new Product("Blue Lawson Armchair","It's blue and nice","",200.00,author1,lawson);
+        Product lawsonRed = new Product("Red Lawson Armchair","It's red and comfortable","",210.99,author1,lawson);
+        Product clubBrown = new Product("Red Club Armchair","It's nice armchair","",150.00,author1,club);
+        Product clubBlue = new Product("Blue Club Armchair","It's just blue","",149.99,author2,club);
+        Product darkWoodWardrobe = new Product("Dark Wood Wardrobe","It's just wardrobe","",199.00,author2,wood);
+        Product birchWoodWardrobe = new Product("Birch Wood Wardrobe","It's just wardrobe","",160.00,author2,wood);
+        Product compositeWardrobe = new Product("Composite Wardrobe","It's just wardrobe","",149.99,author2,composite);
+        Product blackOven = new Product("Black Oven","Oven like from The Sims","",300.99,author3,ovens);
+        Product blackRefrigerators = new Product("Black Refrigerators","Small Refrigerators","",290.99,author3,refrigerators);
+        Product singleCeilingLight = new Product("Single CeilingLight","Small","",90.99,author3,ceilingLighting);
+        Product singleWallLight = new Product("Single WallLight","Small","",90.99,author3,wallLighting);
+
+        if(productList.size()==0){
+            productService.save(lawsonBlue);
+            productService.save(lawsonRed);
+            productService.save(clubBrown);
+            productService.save(clubBlue);
+            productService.save(darkWoodWardrobe);
+            productService.save(birchWoodWardrobe);
+            productService.save(compositeWardrobe);
+            productService.save(blackOven);
+            productService.save(blackRefrigerators);
+            productService.save(singleCeilingLight);
+            productService.save(singleWallLight);
+        }
+
+        List<Address> addressList = addressService.findAll();
+        Address firstAddres = new Address("Poland","Szczecin","Wyszynskiego","12-000");
+
+List<User> userList = userService.findAll();
+        User first = new User("Krzysztof","Stasiak","jedne@ot.pl","123","789456132", ApplicationUserRole.ADMIN,true);
+        User second = new User("Tomasz","Borasz","dwa@ot.pl","987","000000000",ApplicationUserRole.CUSTOMER,true);
+
+        if (userList.size()==0){
+            userService.save(first);
+            userService.save(second);
+        }
+List<Basket> basketList = basketService.findAll();
+        Basket adminBasket = new Basket(first);
+        Basket userBasket = new Basket(second);
+
+        if(basketList.size()==0){
+            basketService.save(adminBasket);
+            basketService.save(userBasket);
+        }
+
+        List<ProductOrderToBasket> productOrderToBasketList = productOrderToBasketService.findAll();
+        ProductOrderToBasket one = new ProductOrderToBasket(first,3,lawsonBlue,adminBasket,20D);
+        ProductOrderToBasket twa = new ProductOrderToBasket(first,2,blackOven,adminBasket,100D);
+
+        if(productOrderToBasketList.size()==0){
+            productOrderToBasketService.save(one);
+            productOrderToBasketService.save(twa);
+        }
     }
 }
